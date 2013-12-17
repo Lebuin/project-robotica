f9382a60d2d8f406ecc978ad457586582336273f
Lebuin <seppe.lenders@gmail.com>
2013-12-17 16:40:34 +0100

    add motion model

--- robot.py
+++ robot.py
@@ -7,6 +7,7 @@ class Robot:
     
     d_sigma = 0.05 # Uncertainty for distances.
     a_sigma = 0.05 # Uncertainty for angles.
+    num_particles = 100
     
     def __init__(self, ang, x, y):
         '''
@@ -20,6 +21,28 @@ class Robot:
         self.x = x
         self.y = y
         self.ang = ang
+        
+        # Initialize particles.
+        
+    
+    def motion_model(self, u, state):
+        '''
+        Calculate the next state for a given state and control.
+        Inputs:
+            u: A tuple of the form (angle, distance) describing the
+                desired movement.
+            state: A tuple of the form (angle, x_coordinate,
+                y_coordinate) describing the current state.
+        Output:
+            A tuple of the form (angle, x_coordinate, y_coordinate).
+        '''
+        
+        ang = state[0] + random.gauss(u[0], self.a_sigma)
+        dist = random.gauss(u[1], u[1] * self.d_sigma)
+        x = state[1] + dist * math.cos(ang)
+        y = state[2] + dist * math.sin(ang)
+        
+        return (ang, x, y)
     
     
     def move(self, ang, dist):
@@ -31,13 +54,12 @@ class Robot:
             dist: The distance over which to move the robot.
         '''
         
-        # Rotate the robot with gaussian distribution.
-        self.ang += random.gauss(ang, self.a_sigma)
+        # Use the motion model to calculate the next state.
+        self.ang, self.x, self.y = self.motion_model(
+            (ang, dist),
+            (self.ang, self.x, self.y)
+        )
         
-        # Move the robot.
-        move = random.gauss(dist, self.d_sigma * dist)
-        self.x += move * math.cos(self.ang)
-        self.y += move * math.sin(self.ang)
         
     
     
