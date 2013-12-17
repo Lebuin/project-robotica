4c347e19314bc7c2e024ad38e6f289e2d4ac88d8
Lebuin <seppe.lenders@gmail.com>
2013-12-17 11:54:48 +0100

    add draw_floor and place_obstacle to map

---
+++ stage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+class Map:
+    
+    def __init__(self, x, y):
+        '''
+        Initialize the map.
+        Inputs:
+            x: the width of the map in pixels
+            y: the height of the map in pixels
+        '''
+        
+        inputs
+        self.x = x
+        self.y = y
+        self.floor = [[0 for i in range(x)] for j in range(y)]
+    
+    def draw_floor(self, num)
+        '''
+        Draw the colours on the floor of the map.
+        Inputs:
+            num: the number of distinctly colored areas
+        '''
+        
+        pass
+        
+    def place_obstacles(self, num)
+        '''
+        Put obstacles on the map.
+        Inputs:
+            num: the number of obstacles to place
+        '''
+        
+        pass
